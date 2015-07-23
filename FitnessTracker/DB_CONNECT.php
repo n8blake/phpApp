8397a69c23f3d4e65e65a76968f66aa4e2dd0e67
Nate <n8blake@mac.com>
2015-07-22 20:39:55 -0500

    add stuff to Fitness Tracker

---
+++ FitnessTracker/DB_CONNECT.php
@@ -0,0 +1,67 @@
+<?php
+//PT location
+//PT Leader
+//PT date/time/temperature
+	/*
+	$username = "root";
+	$password = "root";
+	$host = "localhost:8889";
+	$db = "";*/
+
+	$sql_result;
+
+	$user = 'root';
+	$password = 'root';
+	$db = 'myDB';
+	$host = 'localhost';
+	$port = 8889;
+
+	$link = mysqli_init();
+
+	$conn = new mysqli($host, $user, $password, $db);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+	$success = mysqli_real_connect(
+	   $link, 
+	   $host, 
+	   $user, 
+	   $password, 
+	   $db,
+	   $port
+	);
+
+		
+		//global $sql_result;
+
+		$date = "";
+
+		function set_date($newdate){
+			global $date;
+			$date = $newdate;
+
+		}
+
+		$sql = 'SELECT * from `pt_schedule` WHERE `date`=\'' .$date .'\'';
+
+		$result = $conn->query($sql);
+		$sql_result = $result->fetch_assoc();
+
+		function echo_leader() {
+			global $sql_result;
+			echo $sql_result['leader'];
+		}
+
+		function echo_location(){
+			global $sql_result;
+			echo $sql_result['location'];
+		}
+
+		//var_dump($sql_result);
+		/*foreach ($newresult as $key => $value) {
+			echo "$key : $value";
+		}*/
+
+
+?>
--- FitnessTracker/index.php
+++ FitnessTracker/index.php
@@ -2,7 +2,8 @@
 <html>
 	<head>
 		<title>Fitness Tracker</title>
-		<link rel="stylesheet" type="text/css" href="styles.css">
+		<link rel="stylesheet" type="text/css" href="w3.css">
+		<!--<link rel="stylesheet" type="text/css" href="styles.css">-->
 		<script type="text/javascript"></script>
 	</head>
 	<body>
@@ -10,21 +11,55 @@
 	<?php
 
 		include('weather.php');
+		include('DB_CONNECT.php');
 
 	?>
+		<header class="w3-container <?php echo $flag_condition ?>">
+			<h1 >N3I2 PT Status</h1>
+			<h2>For 
+				<?php
+				$today = getdate();
+				echo $today['weekday'] ." ". $today['month'] ." ". $today['mday'] .", ". $today['year'];
+				
+				if ($today['mon'] < 10){
+					$month = '0'. $today['mon'];
+				} else {$month = $today['mon'];}
 
-		<h1>N3I2 PT Status</h1>
+				$date = $today['year'] ."-".$month."-".$today['mday'];
+				set_date($date);
+				//print_r($today);
+				?>
+			</h2>
+		</header>
 
-		<h2 class="info_header">PT Location: </h2>
-		<p class="info">Medina Base</p>
+		<div class="w3-container">
+			<h3 class="info_header">PT Location: </h3>
+			<p class="info"><?php 
+				//Fetch info for PT leader here;
+				echo_location();
+				//echo "Medina";
+			?>
+			</p>
+		</div>
 
-		<h2 class="info_header">PT Leader: </h2>
-		<p class="info" id="pt_leader">CTI3 Blake</p>
+		<div class="w3-container">
+			<h3 class="info_header">PT Leader: </h3>
+			<p class="info" id="pt_leader"> <?php 
 
-		<h2 class="info_header">The current Flag Condition: </h2>
-		<?php echo '<p class="info" id="flag_color"> ' . $flag_condition .'</p>'; ?>
+				echo_leader();
+	
 
-		<h2>The current temperature in San Antonio is: </h2>
-		<?php echo '<p class="info" id="temp"> ' . $temperature .'</p>'; ?>
+			?> </p>
+		</div>
+
+		<div class="w3-container">
+			<h3 class="info_header">The current Flag Condition: </h3>
+			<?php echo '<p class="info" id="flag_color"> ' . $flag_condition .'</p>'; ?>
+		</div>
+
+		<div class="w3-container">
+			<h3>The current temperature in San Antonio is: </h3>
+			<?php echo '<p class="info" id="temp"> ' . $temperature .'</p>'; ?>
+		</div>
 	</body>
 </html>
